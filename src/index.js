b8d9cf7f8924d2510a253d8a2efe4da94d9a075b
Valeria Matieva <foystina@gmail.com>
2020-03-22 12:12:37 +0400

    feat: upload reverse-int tast on github

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let nn = n.toString();
+    var str = "";
+    for (let i = nn.length-1; i >= 0; i--) {
+      str += nn[i];
+    }
+    return parseInt(str);
 }
